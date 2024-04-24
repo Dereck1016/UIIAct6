c70401ae47da7e578df51f72d2f526763dc2bc60
DERECK_ADRIAN_SANDOVAL <a.21308051280412@cbtis128.edu.mx>
2024-04-24 15:58:06 +0000

    Actualizado

---
+++ lib/data/product_data.dart
@@ -0,0 +1,16 @@
+import 'package:sandoval0412/modelos/product_model.dart';
+
+List<ProductModel> products = [
+  ProductModel(
+      img: "assets/images/onemillon.png",
+      title: "one millon",
+      description: "un perfume con aroma fresco"),
+  ProductModel(
+      img: "assets/images/coach.png",
+      title: "Coach",
+      description: "Un perfume con un olor dulce para fiestas"),
+  ProductModel(
+      img: "assets/images/Dc.png",
+      title: "Dolce Gabbana",
+      description: "Un perfume con olor muy elegante y fresco"),
+];
